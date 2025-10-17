57a0b8fb027a612b62958cbff77da08cb07dacf9
Kitsune7X <ryuizanagi@proton.me>
2025-10-18 00:13:39 +0900

    feat: finish exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times  < 0) {
+        return "ERROR";
+    }
+    let concatString = "";
+    for (let i = 0; i < times; i++) {
+        concatString += `${word}`;
+    }
+    return concatString;
 };
+// console.log("Hello")
 
 // Do not edit below this line
 module.exports = repeatString;
