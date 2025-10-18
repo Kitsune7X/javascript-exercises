8de08425ca10e1ce018ee6bdfd1e050dfa58bd9f
Kitsune7X <ryuizanagi@proton.me>
2025-10-19 06:57:04 +0900

    feat: finish exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr) {
+    // Filter out the element from the array
+    // return arr.filter(item => item !== ele);
+    const args = [...arguments].slice(1);
 
+    return arr.filter((item) => !args.includes(item));
+};
 // Do not edit below this line
 module.exports = removeFromArray;
