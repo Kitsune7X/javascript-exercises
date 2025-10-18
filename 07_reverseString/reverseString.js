260bbe957db3ecc67875a73538d458bb574fa1d0
Kitsune7X <ryuizanagi@proton.me>
2025-10-18 17:29:23 +0900

    feat: complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let reversedArr = [];
+    // Interate through the string from tail to head
+    for (let i = str.length - 1; i >= 0; i--) {
+        // Push all the char into an array
+        reversedArr.push(str[i]);
+    }
+    // Join the elements to a string
+    return reversedArr.join("");
 };
 
 // Do not edit below this line
