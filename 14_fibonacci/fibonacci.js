ba6f643df0a93ef98bae29b584f983c1fe2e137f
Kitsune7X <ryuizanagi@proton.me>
2025-10-27 20:05:18 +0900

    feat: complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  // Valid input check
+  if (Number.isNaN(+number) || number < 0) return "OOPS";
+  let arr = [];
+  if (+number === 0) return 0;
+  for (let i = 0; i < number; i++) {
+    if (i <= 1) {
+      arr.push(1);
+    } else {
+      // Fibonacci formular
+      let y = arr[i - 1] + arr[i - 2];
+      arr.push(y);
+    }
+  }
+  return arr[number - 1];
 };
 
 // Do not edit below this line
