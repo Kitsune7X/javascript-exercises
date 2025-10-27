02df283d4efb6c32125c4d8e92c5c7c1e3ea1408
Kitsune7X <ryuizanagi@proton.me>
2025-10-27 19:22:41 +0900

    feat: organize code and add comments

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,18 +1,18 @@
 const palindromes = function (str) {
   // Remove white spaces, work breaks and punctuation
   // Reverse the string to see if it is the same as the filtered string
-  let newstr = str.toLowerCase().split("");
-  let newarr = newstr.filter(
-    (word) =>
-      (word.charCodeAt(0) >= 97 && word.charCodeAt(0) <= 122) ||
-      (word.charCodeAt(0) >= 48 && word.charCodeAt(0) <= 57)
-  );
-  return newarr.join("") === newarr.reverse().join("");
+  let arrFromStr = str
+    // Make a array of lowercase char from str
+    .toLowerCase()
+    .split("")
+    .filter(
+      // Filter only letters that are either alphabetic or an integer
+      (word) =>
+        (word.charCodeAt(0) >= 97 && word.charCodeAt(0) <= 122) ||
+        (word.charCodeAt(0) >= 48 && word.charCodeAt(0) <= 57)
+    );
+  return arrFromStr.join("") === arrFromStr.reverse().join("");
 };
-
-console.log(palindromes("racecar"));
-
-console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
