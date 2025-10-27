583bc1f0bc4a559710002404e4625e37edc01f98
Kitsune7X <ryuizanagi@proton.me>
2025-10-27 20:42:10 +0900

    feat: did stuff

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // Sort the array first then reduce
+  return people.reduce((acc, curr) => {
+    acc["name"] = curr.name;
+    acc["yearOfBirth"] = curr.yearOfBirth;
+    acc[Object.keys(curr)[2]] = curr.yearOfDeath;
+    return acc;
+  }, {});
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
