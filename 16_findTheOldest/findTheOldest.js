d899a8a4dc6179974a10e6228c3c37d96263af8e
Kitsune7X <ryuizanagi@proton.me>
2025-10-28 19:59:43 +0900

    feat: complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,32 +1,31 @@
 const findTheOldest = function (people) {
-  // Sort the array first then reduce
-  return people.reduce((acc, curr) => {
-    acc["name"] = curr.name;
-    acc["yearOfBirth"] = curr.yearOfBirth;
-    acc[Object.keys(curr)[2]] = curr.yearOfDeath;
-    return acc;
-  }, {});
+  // Get current year
+  const currentYear = new Date().getFullYear();
+
+  // Make sure all object have all the necessary keys
+  const peopleMapped = people.map(({ name, yearOfBirth, yearOfDeath }) => {
+    if (!yearOfDeath) {
+      yearOfDeath = currentYear;
+    }
+    return { name, yearOfBirth, yearOfDeath };
+  });
+
+  // Sort the array by years lived in ascending order
+  return (
+    peopleMapped
+      .sort(
+        (a, b) =>
+          a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth)
+      )
+      // Reduce the array to an object which is the person with greatest age
+      .reduce((acc, curr) => {
+        acc[Object.keys(curr)[0]] = curr.name;
+        acc[Object.keys(curr)[1]] = curr.yearOfBirth;
+        acc[Object.keys(curr)[2]] = curr.yearOfDeath;
+        return acc;
+      }, {})
+  );
 };
-
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-];
-
-console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
